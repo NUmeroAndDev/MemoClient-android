ec03dd2b1d1cd315ce62d846c05bb77842526dca
NUmeroAndDev <numero.dev.and@gmail.com>
2016-09-06 12:27:32 +0900

    delete comment

--- app/src/main/java/com/numero/memoclient/Views/LoadingProgressDialog.java
+++ app/src/main/java/com/numero/memoclient/Views/LoadingProgressDialog.java
@@ -11,7 +11,6 @@ public class LoadingProgressDialog {
 
     public LoadingProgressDialog(Context context){
         dialog = new ProgressDialog(context);
-//        ToDo: get text from res
         dialog.setMessage(context.getString(R.string.message_loading));
         dialog.setCanceledOnTouchOutside(false);
     }
