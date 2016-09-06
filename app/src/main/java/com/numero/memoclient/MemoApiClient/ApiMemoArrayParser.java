369e3ea8da32a96dd4b4cb37dcf5d9c4c6014579
NUmeroAndDev <numero.dev.and@gmail.com>
2016-09-06 11:20:34 +0900

    fixed parser

--- app/src/main/java/com/numero/memoclient/MemoApiClient/ApiMemoArrayParser.java
+++ app/src/main/java/com/numero/memoclient/MemoApiClient/ApiMemoArrayParser.java
@@ -1,7 +1,6 @@
 package com.numero.memoclient.MemoApiClient;
 
-import android.os.Handler;
-import android.os.Message;
+import android.os.AsyncTask;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,8 +8,9 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class ApiMemoArrayParser {
+public class ApiMemoArrayParser extends AsyncTask<Void, Void, Void> {
 
+    private Callback callback;
     private ArrayList<Memo> memoList;
     private String data;
 
@@ -27,37 +27,34 @@ public class ApiMemoArrayParser {
         return this;
     }
 
-    public void execute(final Callback callback){
-        final Handler handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message message) {
-                callback.onPostExecute();
-                return false;
-            }
-        });
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    JSONArray jsonArray = new JSONArray(data);
-                    for (int i = 0; i < jsonArray.length(); i++){
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+    public void execute(Callback callback){
+        this.callback = callback;
+        this.execute();
+    }
 
-                        memoList.add(Memo.init()
-                                .setID(jsonObject.getLong("id"))
-                                .setValue(jsonObject.getString("value"))
-                                .setCreated(jsonObject.getString("created_at"))
-                                .setUpdate(jsonObject.getString("updated_at"))
-                                .setURL(jsonObject.getString("url"))
-                        );
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                handler.sendMessage(Message.obtain());
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            JSONArray jsonArray = new JSONArray(data);
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                memoList.add(Memo.init()
+                        .setID(jsonObject.getLong("id"))
+                        .setValue(jsonObject.getString("value"))
+                        .setCreated(jsonObject.getString("created_at"))
+                        .setUpdate(jsonObject.getString("updated_at"))
+                        .setURL(jsonObject.getString("url"))
+                );
             }
-        });
-        thread.start();
+        } catch (JSONException ignored) {
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void v) {
+        callback.onPostExecute();
     }
 
     public interface Callback{
--- app/src/main/java/com/numero/memoclient/MemoApiClient/ApiMemoParser.java
+++ app/src/main/java/com/numero/memoclient/MemoApiClient/ApiMemoParser.java
@@ -1,13 +1,13 @@
 package com.numero.memoclient.MemoApiClient;
 
-import android.os.Handler;
-import android.os.Message;
+import android.os.AsyncTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ApiMemoParser {
+public class ApiMemoParser extends AsyncTask<Void, Void, Void>{
 
+    private Callback callback;
     private String data;
     private Memo memo;
 
@@ -19,34 +19,31 @@ public class ApiMemoParser {
         return new ApiMemoParser(data);
     }
 
-    public void execute(final Callback callback) {
-        final Handler handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message message) {
-                callback.onPostExecute(memo);
-                return false;
-            }
-        });
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    JSONObject jsonObject = new JSONObject(data);
+    public void execute(Callback callback){
+        this.callback = callback;
+        this.execute();
+    }
 
-                    memo = Memo.init()
-                            .setID(jsonObject.getLong("id"))
-                            .setValue(jsonObject.getString("value"))
-                            .setCreated(jsonObject.getString("created_at"))
-                            .setUpdate(jsonObject.getString("updated_at"))
-                            .setURL(jsonObject.getString("url"));
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                handler.sendMessage(Message.obtain());
-            }
-        });
-        thread.start();
+            memo = Memo.init()
+                    .setID(jsonObject.getLong("id"))
+                    .setValue(jsonObject.getString("value"))
+                    .setCreated(jsonObject.getString("created_at"))
+                    .setUpdate(jsonObject.getString("updated_at"))
+                    .setURL(jsonObject.getString("url"));
+
+        } catch (JSONException ignored) {
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void v) {
+        callback.onPostExecute(memo);
     }
 
     public interface Callback {
